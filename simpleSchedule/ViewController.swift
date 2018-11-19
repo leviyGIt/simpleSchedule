203f1a75fc2617c3e07edf1dccc31503173b1cae
kirshum <kirshum@list.ru>
2018-11-19 21:42:58 +0300

    Initial Commit

---
+++ simpleSchedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpleSchedule
+//
+//  Created by kirshum on 19/11/2018.
+//  Copyright © 2018 kirshum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
